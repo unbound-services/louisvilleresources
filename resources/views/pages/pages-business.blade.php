ace7bfe24f5af6da57df3f3975c4a2c75e98139c
apollyphon <lowkee3est@gmail.com>
2020-03-25 20:24:06 -0400

    initial list and business page listing

---
+++ resources/views/pages/pages-business.blade.php
@@ -0,0 +1,10 @@
+
+@extends('pages.layouts.pages-layout')
+
+@section('content')
+
+  <section class="business-info">
+    <h1 class="business-title">{{$business->name}}</h1>
+  </section>
+
+@endsection
--- routes/web.php
+++ routes/web.php
@@ -14,8 +14,8 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', 'HomeController@index');
-
-
+Route::get('/directory', 'BusinessDirectoryController@index');
+Route::get('/directory/{business}', 'BusinessDirectoryController@view');
 
 
 Route::get('about', 'HomeController@about');
